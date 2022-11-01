9f04c8436c509a9b075a1560f720465fc3c4fcd9
robertocajueiro <beto.cajueiro@gmail.com>
2022-11-01 00:31:35 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CidadeService } from './cidade.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'app-cadastro';
+
+  cidades = [
+    { id: 1, nome: 'Recife'}
+  ];
+
+  constructor(private cidadeService: CidadeService){
+
+  }
+
+  ngOnInit(){
+    this.cidadeService.consultar();
+  }
+
+  adicionar(nome: string) {
+    alert(nome);
+  }
+
+  excluir(id: number) {
+    alert(id);
+  }
+
+  atualizar(cidade: any) {
+    alert(JSON.stringify(cidade));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CidadeService } from './cidade.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CidadeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
